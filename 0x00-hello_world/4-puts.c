ba017990327ed104a45beaa7303d7907c5b9d8af
Osi Enekhaze <c.o.enekhaze@gmail.com>
2022-06-30 12:37:29 -0700

    task_d

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+* main- entry point
+*return: always 0 (Successful)
+*/
+
+int main(coid)
+{
+   printf("\"Programming is like building a multilingual puzzle\n");
+   return (0);
+}
