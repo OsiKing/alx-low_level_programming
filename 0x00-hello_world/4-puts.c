67ea7dd3a5d9894d5889f3caeefdeb544705def9
Osi Enekhaze <c.o.enekhaze@gmail.com>
2022-06-30 12:49:28 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-* main- entry point
-*return: always 0 (Successful)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(coid)
+int main(void)
 {
-   printf("\"Programming is like building a multilingual puzzle\n");
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
