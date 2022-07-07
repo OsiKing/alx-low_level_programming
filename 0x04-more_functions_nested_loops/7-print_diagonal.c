bebf54d348a18ee2436dd90d47aca9160892456e
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2022-07-07 23:22:57 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,21 +2,21 @@
 
 /**
  * print_line - Draws a diagonal line using the character \.
- * @n: The number of _ characters to be printed.
+ * @n: The number of \ characters to be printed.
  **/
 void print_diagonal(int n)
 {
-	int dish, space;
+	int len, space;
 
 	if (n > 0)
 	{
-		for (dish = 0; dish < n; dish++)
+		for (len = 0; len < n; len++)
 		{
-			for (space = 0; space < dish; space++)
+			for (space = 0; space < len; space++)
 				_putchar(' ');
 			_putchar('\\');
 
-			if (dish == n - 1)
+			if (len == n - 1)
 				continue;
 
 			_putchar('\n');
