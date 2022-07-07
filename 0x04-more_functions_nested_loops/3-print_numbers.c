b324342d6e90dd37068b08383a89c317e548f9c9
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2022-07-07 22:25:24 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 
 /**
- *print_numbers - Prints the numbers from 0-9.
+ * print_numbers - Prints the numbers from 0-9.
  **/
 void print_numbers(void)
 {
@@ -13,4 +13,3 @@ void print_numbers(void)
 	
 	_putchar('\n');
 }
-
