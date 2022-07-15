32d245d514bb0618678d98cfbd566c0ddbbe0e5b
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2022-07-15 03:46:00 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 /**
- *main.h
- *Desc: Header file containing prototypes for all functions
- *used in the 0x06-pointers_arrays_strings directory.
- **/
+ * File: holberton.h
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_arrays_strings directory.
+ */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -17,3 +20,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
