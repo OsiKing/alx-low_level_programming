7df16f920caa3ae960084fbbcbb787f2a7051cb7
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2022-07-29 16:06:08 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,7 @@ char *_strdup(char *str)
 {
 	char *duplicate;
 	int index, len = 0;
-	
+
 	if (str == NULL)
 		return (NULL);
 
@@ -22,7 +22,7 @@ char *_strdup(char *str)
 	duplicate = malloc(sizeof(char) * (len + 1));
 
 	if (duplicate == NULL)
-		retrun (NULL);
+		return (NULL);
 
 	for (index = 0; str[index]; index++)
 		duplicate[index] = str[index];
