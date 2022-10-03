ee176474ca268e55ea5f3e9ea4577cac2c401313
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2022-10-03 23:00:35 +0100

    Update 100-operations.c

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
-  int first, second, add, subtract, multiply;
-  float divide;
+int first, second, add, subtract, multiply;
+float divide;
+printf("Enter two integers\n");
+scanf("%d%d", &first, &second);
+add = first + second;
+subtract = first - second;
+multiply = first * second;
+divide = first / (float)second;
+printf("Sum = %d\n", add);
+printf("Difference = %d\n", subtract);
+printf("Multiplication = %d\n", multiply);
+printf("Division = %.2f\n", divide);
  
-  printf("Enter two integers\n");
-  scanf("%d%d", &first, &second);
- 
-  add = first + second;
-  subtract = first - second;
-  multiply = first * second;
-  divide = first / (float)second;
-
-  printf("Sum = %d\n", add);
-  printf("Difference = %d\n", subtract);
-  printf("Multiplication = %d\n", multiply);
-  printf("Division = %.2f\n", divide);
- 
-  return 0;
+return 0;
 }
