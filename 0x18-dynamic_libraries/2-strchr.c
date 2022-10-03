d6eadb425464c364feb8263e7a98ca9ba4e5e567
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2022-10-03 23:45:50 +0100

    2-strchr.c

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string given
+ * @c: searchen character
+ *
+ * Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
+else
+{
+return (NULL);
+}
+return ('\0');
+}
